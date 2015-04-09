8a58113bc4271adc4ce001f904c56fb45f314ecd
Vassil Mladenov <vmladenov@icloud.com>
2015-04-09 01:19:55 -0500

    added a function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ function validate_nodes() {
 	console.log("Hello World!");
 	return true;
 }
+
+function getNumNodes(input_string) {
+	input_string = input_string.toUpperCase();
+	var arr = input_string.split(",");
+	for (var i = 0; i < arr.length; i++) {
+		var elem = arr[i];
+
+		if (elem.length != 1) {
+			console.log("Please only single letters.")
+			return -1;
+		}
+	};
+	return arr.length;
+}

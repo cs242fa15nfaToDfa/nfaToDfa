9afc7a2dc0b164304ed62e9bbaf93db19a052a4b
Ash <ashkumar12@yahoo.com>
2015-04-16 16:53:27 -0500

    added setTransition test

--- tests/tests.js
+++ tests/tests.js
@@ -9,3 +9,12 @@ QUnit.test( "testStateConstructor", function( assert ) {
 	var testState = new State("test");
 	assert.equal(testState.name == "test", true);
 });
+
+
+QUnit.test( "testSetTransitionFunction", function ( assert ) {
+	var testState = new State("test");
+	var transitionStates = ["m"];
+	testState.setTransition("n", transitionStates);
+	console.log(testState.adjacencyList["n"]);
+	assert.equal(testState.adjacencyList["n"][0] == "m", true);
+});

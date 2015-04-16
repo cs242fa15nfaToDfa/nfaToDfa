613896bf65cc39cde1be13b90d0d5c0e58a7c6e0
Ash <ashkumar12@yahoo.com>
2015-04-16 18:07:04 -0500

    updated tests syntax

--- tests/tests.js
+++ tests/tests.js
@@ -7,7 +7,7 @@ QUnit.test( "hello test", function( assert ) {
 
 QUnit.test( "testStateConstructor", function( assert ) {
 	var testState = new State("test");
-	assert.equal(testState.name, "test", true);
+	assert.equal(testState.name, "test");
 });
 
 
@@ -16,27 +16,27 @@ QUnit.test( "testSetTransitionFunction", function ( assert ) {
 	var transitionStates = ["i", "j"];
 	testState.setTransition("n", transitionStates);
 	console.log(testState.adjacencyList["n"]);
-	assert.equal(testState.adjacencyList["n"][0],"i", true);
-	assert.equal(testState.adjacencyList["n"][1],"j", true)
+	assert.equal(testState.adjacencyList["n"][0],"i");
+	assert.equal(testState.adjacencyList["n"][1],"j")
 });
 
 
 QUnit.test( "testGetStatesEmpty", function ( assert ) {
 	var testResult = getStates("");
-	assert.equal(testResult.length, 0 , true);
+	assert.equal(testResult.length, 0 );
 });
 
 
 QUnit.test( "testGetStates", function ( assert ){
 	var testResult = getStates("i,j,k");
-	assert.equal(testResult[0],"i", true);
-	assert.equal(testResult[1],"j", true);
-	assert.equal(testResult[2],"k", true);
+	assert.equal(testResult[0],"i");
+	assert.equal(testResult[1],"j");
+	assert.equal(testResult[2],"k");
 });
 
 QUnit.test( "testBuildJSON", function ( assert ){
 	var testState = new State("test");
 	var testArray = [testState];
 	var testJSONArr = buildJSON(testArray);
-	assert.equal(testJSONArr.nodes[0].name,"test", true);
+	assert.equal(testJSONArr.nodes[0].name,"test");
 });

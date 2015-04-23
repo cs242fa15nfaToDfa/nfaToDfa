7f32d0776b21b3e35aa23740f5e59a1973f549bc
Vassil Mladenov <vmladenov@icloud.com>
2015-04-23 16:16:16 -0500

    renamed State to NFAState, commented extensively

--- app/transform.php
+++ app/transform.php
@@ -2,14 +2,15 @@
 	require("state.php");
 	require("json.php");
 
+	// decode the json from the server
 	$json = file_get_contents('php://input');
 	$obj = json_decode($json);
-
+	// extract the contents of the json and feed them into the preprocessing functions
 	$stateNames = $obj->stateNames;
 	$transitions = $obj->transitions;
 	$nfaStates = jsonToStateArray($obj->states);
-
+	
+	// perform the subset transformation and return to the client
 	$dfaStates = transformToDfa($stateNames, $transitions, $nfaStates);
-
 	echo buildJSON($dfaStates);
 ?>
